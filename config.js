e2c461b70aedc0be09629915cfcb97fb81dff2bc
Pete Birley <petebirley@gmail.com>
2015-03-25 09:17:45 +0000

    Update config.js

--- config.js
+++ config.js
@@ -4,16 +4,16 @@ exports.host = null;
 // string to specify this server's ip, or null to listen on any ip
 exports.ip = null;
 
-exports.port = process.env.PORT || 80;
+exports.port = 80;
 
 // if set, GA tracking code will be inserted into the bottom of every page
-exports.google_analytics_id = process.env.GA_ID || null;
+exports.google_analytics_id = null;
 
 // this is used connect to keep session cookies secure. You should change this.
-exports.secret = process.env.SECRET || "correct horse battery staple";
+exports.secret = "correct horse battery staple";
 
 // for redis if you're on heroku or using redis-to-go
-exports.redistogo_url = process.env.REDISTOGO_URL || "";
+exports.redistogo_url = "";
 
 // for redis if you're running your own copy
 exports.redis_host = "localhost";
